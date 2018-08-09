de1c2485017fe09bcef9c8b12aa2db4dcbf1f946
Martin Bernardi (Laptop) <martinber.710@gmail.com>
2018-08-09 19:45:52 -0300

    Working for 20800Hz WAV files

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,21 @@ fn main() -> hound::Result<()> {
     simple_logger::init().unwrap();
 
     debug!("Leyendo WAV:");
-    let (input_signal, input_spec) = wav::load_wav("./11025.wav");
+    let (input_signal, input_spec) = wav::load_wav("./20800.wav");
     debug!("Cargado WAV en Vec");
     debug!("reader_spec: {:?}", input_spec);
 
     let max: &f32 = dsp::get_max(&input_signal);
     debug!("Maximo: {}", max);
 
-    let r = 7/3; // resampling factor
-    let l = 1; // interpolation
-    let m = 1; // decimation
     let atten = 50.;
     let delta_w = 1./20.;
-    let resampled = dsp::resample(&input_signal, l, m, atten, delta_w);
+    let demodulated = dsp::demodulate(&input_signal, atten, delta_w);
+
+    let r = 7/3; // resampling factor
+    let l = 1; // interpolation
+    let m = 5;
+    let resampled = dsp::resample(&demodulated, l, m, atten, delta_w);
 
     let writer_spec = hound::WavSpec {
         channels: 1,
@@ -40,12 +42,10 @@ fn main() -> hound::Result<()> {
         sample_format: hound::SampleFormat::Float,
     };
 
-    let demodulated = dsp::demodulate(&resampled, atten, delta_w);
-
     println!("Resampleado");
-    let max = dsp::get_max(&demodulated);
+    let max = dsp::get_max(&resampled);
     println!("Maximo: {}", max);
-    let normalized = demodulated.iter().map(|x| x/max).collect();
+    let normalized = resampled.iter().map(|x| x/max).collect();
 
     println!("Escribiendo WAV:");
     wav::write_wav("./salida.wav", &normalized, writer_spec);
@@ -63,11 +63,11 @@ fn main() -> hound::Result<()> {
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(w, input_spec.sample_rate/2, 700);
+    let mut encoder = png::Encoder::new(w, 2080, 700);
     encoder.set(png::ColorType::Grayscale).set(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
 
-    writer.write_image_data(&acomodado[0..(input_spec.sample_rate/2*700) as usize]).unwrap(); // Save
+    writer.write_image_data(&acomodado[0..(2080*700) as usize]).unwrap(); // Save
 
     /*
     // let window = dsp::kaiser(40., 1./10.);
