bfbdbc56f9d4045f52a62f097582d82c00b3e8ef
Martin Bernardi (PC) <martinber.710@gmail.com>
2018-08-19 20:55:08 -0300

    Finished err::Error

--- src/err.rs
+++ src/err.rs
@@ -1,8 +1,10 @@
-use std::fmt;
-
-use std::io;
+use std;
 use hound;
 use log;
+
+// So I can use std::error::Error::description(), but I don't want to shadow
+// Error in this scope
+use std::error::Error as StdError;
 
 #[derive(Debug)]
 enum Error {
@@ -12,24 +14,24 @@ enum Error {
     Internal(String), // noaa-apt internal errors because of some bug
 }
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
             Error::Io(ref err) => err.fmt(f),
-            Error::WavOpen(msg) => f.write_str(msg.to_str()),
-            Error::PngWrite(msg) => f.write_str(msg.to_str()),
-            Error::Internal(msg) => f.write_str(msg.to_str()),
+            Error::WavOpen(ref msg) => f.write_str(msg.as_str()),
+            Error::PngWrite(ref msg) => f.write_str(msg.as_str()),
+            Error::Internal(ref msg) => f.write_str(msg.as_str()),
         }
     }
 }
 
-impl error::Error for Error {
+impl std::error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::Io(ref err) => err.description(),
-            Error::WavOpen(msg) => msg.to_str(),
-            Error::PngWrite(msg) => msg.to_str(),
-            Error::Internal(msg) => msg.to_str(),
+            Error::WavOpen(ref msg) => msg.as_str(),
+            Error::PngWrite(ref msg) => msg.as_str(),
+            Error::Internal(ref msg) => msg.as_str(),
         }
     }
 }
@@ -43,18 +45,18 @@ impl From<std::io::Error> for Error {
 impl From<hound::Error> for Error {
     fn from(err: hound::Error) -> Error {
         match err {
-            hound::Error::IoError(io_error) => Io(io_error),
-            hound::Error::FormatError => WavOpen(err.to_string()),
-            hound::Error::TooWide => Internal(err.to_string()),
-            hound::Error::UnfinishedSample => Internal(err.to_string()),
-            hound::Error::Unsupported => WavOpen(err.to_string()),
-            hound::Error::InvalidSampleFormat => Internal(err.to_string()),
+            hound::Error::IoError(io_error) => Error::Io(io_error),
+            hound::Error::FormatError(_) => Error::WavOpen(err.to_string()),
+            hound::Error::TooWide => Error::Internal(err.to_string()),
+            hound::Error::UnfinishedSample => Error::Internal(err.to_string()),
+            hound::Error::Unsupported => Error::WavOpen(err.to_string()),
+            hound::Error::InvalidSampleFormat => Error::Internal(err.to_string()),
         }
     }
 }
 
 impl From<log::SetLoggerError> for Error {
     fn from(err: log::SetLoggerError) -> Error {
-        Internal(err.description())
+        Error::Internal(err.description().to_string())
     }
 }
