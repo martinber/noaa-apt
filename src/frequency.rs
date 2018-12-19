1e741200b3a803cf6e9f677f0f35d6164bfd6411
Martin Bernardi (PC) <martinber.710@gmail.com>
2018-12-18 21:35:58 -0300

    Operator overloading for Frequency

--- src/frequency.rs
+++ src/frequency.rs
@@ -1,5 +1,15 @@
 use std::f32::consts::PI;
 
+use std::ops::Add;
+use std::ops::Sub;
+use std::ops::Mul;
+use std::ops::Div;
+use std::ops::AddAssign;
+use std::ops::SubAssign;
+use std::ops::MulAssign;
+use std::ops::DivAssign;
+
+#[derive(Copy, Clone, Debug)]
 pub struct Frequency {
     pi_radians: f32
 }
@@ -36,76 +46,242 @@ impl Frequency {
     }
 }
 
+// Operators against Frequencies
+
+impl Add for Frequency {
+    type Output = Frequency;
+
+    fn add(self, other: Frequency) -> Frequency {
+        Frequency { pi_radians: self.pi_radians + other.pi_radians }
+    }
+}
+
+impl Sub for Frequency {
+    type Output = Frequency;
+
+    fn sub(self, other: Frequency) -> Frequency {
+        Frequency { pi_radians: self.pi_radians - other.pi_radians }
+    }
+}
+
+impl Mul for Frequency {
+    type Output = Frequency;
+
+    fn mul(self, other: Frequency) -> Frequency {
+        Frequency { pi_radians: self.pi_radians * other.pi_radians }
+    }
+}
+
+impl Div for Frequency {
+    type Output = Frequency;
+
+    fn div(self, other: Frequency) -> Frequency {
+        Frequency { pi_radians: self.pi_radians / other.pi_radians }
+    }
+}
+
+// Operators against f32
+
+impl Mul<f32> for Frequency {
+    type Output = Frequency;
+
+    fn mul(self, other: f32) -> Frequency {
+        Frequency { pi_radians: self.pi_radians * other }
+    }
+}
+
+impl Div<f32> for Frequency {
+    type Output = Frequency;
+
+    fn div(self, other: f32) -> Frequency {
+        Frequency { pi_radians: self.pi_radians / other }
+    }
+}
+
+// Assign operators against Frequencies
+
+impl AddAssign for Frequency {
+    fn add_assign(&mut self, other: Frequency) {
+        self.pi_radians += other.pi_radians;
+    }
+}
+
+impl SubAssign for Frequency {
+    fn sub_assign(&mut self, other: Frequency) {
+        self.pi_radians -= other.pi_radians;
+    }
+}
+
+impl MulAssign for Frequency {
+    fn mul_assign(&mut self, other: Frequency) {
+        self.pi_radians *= other.pi_radians;
+    }
+}
+
+impl DivAssign for Frequency {
+    fn div_assign(&mut self, other: Frequency) {
+        self.pi_radians /= other.pi_radians;
+    }
+}
+
+// Assign operators against f32
+
+impl MulAssign<f32> for Frequency {
+    fn mul_assign(&mut self, other: f32) {
+        self.pi_radians *= other;
+    }
+}
+
+impl DivAssign<f32> for Frequency {
+    fn div_assign(&mut self, other: f32) {
+        self.pi_radians /= other;
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
     use super::*;
 
+    /// Has equivalences between values on different units
+    #[derive(Debug)]
+    struct Equivalence {
+        pi_radians: f32,
+        radians: f32,
+        hertz: f32,
+        rate: f32
+    }
+
+    const TEST_VALUES: [Equivalence; 11] = [
+        Equivalence {
+            pi_radians: 0.435374149659864,
+            radians: 1.367768230134332,
+            hertz: 2400.,
+            rate: 11025.
+        },
+        Equivalence {
+            pi_radians: -0.435374149659864,
+            radians: -1.367768230134332,
+            hertz: -2400.,
+            rate: 11025.
+        },
+        Equivalence {
+            pi_radians: 0.1,
+            radians: 0.3141592653589793,
+            hertz: 100.,
+            rate: 2000.
+        },
+        Equivalence {
+            pi_radians: -0.1,
+            radians: -0.3141592653589793,
+            hertz: -100.,
+            rate: 2000.
+        },
+        Equivalence { pi_radians: 0., radians: 0., hertz: 0., rate: 11025. },
+        Equivalence { pi_radians: 1., radians: PI, hertz: 5512.5, rate: 11025. },
+        Equivalence { pi_radians: -1., radians: -PI, hertz: -5512.5, rate: 11025. },
+        Equivalence { pi_radians: 2., radians: 2.*PI, hertz: 11025., rate: 11025. },
+        Equivalence { pi_radians: -2., radians: -2.*PI, hertz: -11025., rate: 11025. },
+        Equivalence { pi_radians: 300., radians: 300.*PI, hertz: 150., rate: 1. },
+        Equivalence { pi_radians: -300., radians: -300.*PI, hertz: -150., rate: 1. },
+    ];
+
+
     /// Check if two floats are equal given some margin of precision
-    fn roughly_equal(a: &f32, b: &f32) -> bool {
-        ulps_eq!(a, b, max_ulps = 10)
+    fn assert_roughly_equal(a: f32, b: f32) {
+        assert_ulps_eq!(a, b, max_ulps = 10)
     }
 
     #[test]
-    fn test_pi_radians() {
-        let f = Frequency::pi_radians(0.1234);
-        assert!(roughly_equal(&f.get_pi_radians(), &0.1234));
-        let f = Frequency::pi_radians(1.);
-        assert!(roughly_equal(&f.get_pi_radians(), &1.));
-        let f = Frequency::pi_radians(0.);
-        assert!(roughly_equal(&f.get_pi_radians(), &0.));
-
-        // Test values larger than one and smaller than zero too
-        let f = Frequency::pi_radians(123.);
-        assert!(roughly_equal(&f.get_pi_radians(), &123.));
-        let f = Frequency::pi_radians(-1.);
-        assert!(roughly_equal(&f.get_pi_radians(), &-1.));
-        let f = Frequency::pi_radians(-0.6);
-        assert!(roughly_equal(&f.get_pi_radians(), &-0.6));
+    fn test_frequency_conversion() {
+        for e in TEST_VALUES.iter() {
+            let f = Frequency::pi_radians(e.pi_radians);
+            assert_roughly_equal(f.get_pi_radians(), e.pi_radians);
+            assert_roughly_equal(f.get_radians(), e.radians);
+            assert_roughly_equal(f.get_hertz(e.rate), e.hertz);
+            let f = Frequency::radians(e.radians);
+            assert_roughly_equal(f.get_pi_radians(), e.pi_radians);
+            assert_roughly_equal(f.get_radians(), e.radians);
+            assert_roughly_equal(f.get_hertz(e.rate), e.hertz);
+            let f = Frequency::hertz(e.hertz, e.rate);
+            assert_roughly_equal(f.get_pi_radians(), e.pi_radians);
+            assert_roughly_equal(f.get_radians(), e.radians);
+            assert_roughly_equal(f.get_hertz(e.rate), e.hertz);
+        }
     }
 
     #[test]
-    fn test_radians() {
-        let f = Frequency::radians(1.1234);
-        assert!(roughly_equal(&f.get_radians(), &1.1234));
-        let f = Frequency::radians(PI);
-        assert!(roughly_equal(&f.get_radians(), &PI));
-        assert!(roughly_equal(&f.get_pi_radians(), &1.));
-        let f = Frequency::radians(0.);
-        assert!(roughly_equal(&f.get_radians(), &0.));
-        assert!(roughly_equal(&f.get_pi_radians(), &0.));
+    fn test_operations() {
+        let a: f32 = 12345.;
+        let b: f32 = -23456.;
 
-        // Test values larger than pi and smaller than zero too
-        let f = Frequency::radians(123.);
-        assert!(roughly_equal(&f.get_radians(), &123.));
-        let f = Frequency::radians(-PI);
-        assert!(roughly_equal(&f.get_radians(), &-PI));
-        assert!(roughly_equal(&f.get_pi_radians(), &-1.));
-        let f = Frequency::radians(-2.6);
-        assert!(roughly_equal(&f.get_radians(), &-2.6));
+        let fa = Frequency::pi_radians(a);
+        let fb = Frequency::pi_radians(b);
+
+        // Operators against Frequencies
+
+        assert_roughly_equal(
+            fa.get_pi_radians() + fb.get_pi_radians(),
+            (fa + fb).get_pi_radians());
+        assert_roughly_equal(
+            fa.get_pi_radians() - fb.get_pi_radians(),
+            (fa - fb).get_pi_radians());
+        assert_roughly_equal(
+            fa.get_pi_radians() * fb.get_pi_radians(),
+            (fa * fb).get_pi_radians());
+        assert_roughly_equal(
+            fa.get_pi_radians() / fb.get_pi_radians(),
+            (fa / fb).get_pi_radians());
+
+        // Operators against f32
+
+        assert_roughly_equal(
+            fa.get_pi_radians() * b,
+            (fa * b).get_pi_radians());
+        assert_roughly_equal(
+            fa.get_pi_radians() / b,
+            (fa / b).get_pi_radians());
+
+        // Assign operators against frequencies
+
+        let fb = Frequency::pi_radians(b);
+
+        let mut fa = Frequency::pi_radians(a);
+        fa += fb;
+        assert_roughly_equal(fa.get_pi_radians(), a + b);
+
+        let mut fa = Frequency::pi_radians(a);
+        fa -= fb;
+        assert_roughly_equal(fa.get_pi_radians(), a - b);
+
+        let mut fa = Frequency::pi_radians(a);
+        fa *= fb;
+        assert_roughly_equal(fa.get_pi_radians(), a * b);
+
+        let mut fa = Frequency::pi_radians(a);
+        fa /= fb;
+        assert_roughly_equal(fa.get_pi_radians(), a / b);
+
+        // Assign operators against f32
+
+        let mut fa = Frequency::pi_radians(a);
+        fa *= b;
+        assert_roughly_equal(fa.get_pi_radians(), a * b);
+
+        let mut fa = Frequency::pi_radians(a);
+        fa /= b;
+        assert_roughly_equal(fa.get_pi_radians(), a / b);
     }
 
     #[test]
-    fn test_hertz() {
-        let f = Frequency::hertz(2400., 11025.);
-        assert!(roughly_equal(&f.get_hertz(11025.), &2400.));
-        assert!(roughly_equal(&f.get_pi_radians(), &0.435374149659864));
-        let f = Frequency::hertz(5512.5, 11025.);
-        assert!(roughly_equal(&f.get_hertz(11025.), &5512.5));
-        assert!(roughly_equal(&f.get_pi_radians(), &1.));
-        let f = Frequency::hertz(0., 11025.);
-        assert!(roughly_equal(&f.get_hertz(11025.), &0.));
-        assert!(roughly_equal(&f.get_pi_radians(), &0.));
+    #[allow(unused_assignments, unused_mut)]
+    fn test_copying() {
+        let mut a = Frequency::pi_radians(123.);
+        let mut b = Frequency::pi_radians(456.);
 
-        // Test values larger than rate and smaller than zero too
-        let f = Frequency::hertz(11025., 11025.);
-        assert!(roughly_equal(&f.get_hertz(11025.), &11025.));
-        assert!(roughly_equal(&f.get_pi_radians(), &2.));
-        let f = Frequency::hertz(-5512.5, 11025.);
-        assert!(roughly_equal(&f.get_hertz(11025.), &-5512.5));
-        assert!(roughly_equal(&f.get_pi_radians(), &-1.));
-        let f = Frequency::hertz(-2400., 11025.);
-        assert!(roughly_equal(&f.get_hertz(11025.), &-2400.));
-        assert!(roughly_equal(&f.get_pi_radians(), &-0.435374149659864));
+        b = a;
+
+        assert_roughly_equal(b.get_pi_radians(), 123.);
+        assert_roughly_equal(b.get_pi_radians(), 123.);
     }
 }
